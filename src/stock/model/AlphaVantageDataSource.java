ee8502884d11736892d7bd58ad88a3d6fbcc2a3f
cindy <322169xyu@gmail.com>
2024-06-03 16:19:55 -0400

    updates

--- src/stock/model/AlphaVantageDataSource.java
+++ src/stock/model/AlphaVantageDataSource.java
@@ -100,69 +100,7 @@ public class AlphaVantageDataSource extends CSVDataSource {
 
     return true;
   }
-
-  //  public void initialize() {
-//    File folder = new File("res/APIData");
-//    generateStockCSV(folder, "A");
-//    loadAllStockData("res/APIData");
-//  }
-
-
-//  @Override
-//  protected void loadAllStockData(String directoryPath) {
-//    try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
-//      paths.filter(Files::isRegularFile)
-//              .filter(path -> path.toString().endsWith(".csv"))
-//              .forEach(this::addAllStocksToList);
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-
-//  }
-
-//  private void addAllStocksToList(Path filePath) {
-//    try (BufferedReader reader = new BufferedReader(new FileReader(filePath.toFile()))) {
-//      String line = reader.readLine();
-//      while ((line = reader.readLine()) != null) {
-//        String[] parts = line.split(",");
-//        String ticker = parts[0];
-//        stockSymbols.add(ticker);
-//      }
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-//  }
-
-//  private void generateStockCSV(File folder) {
-//    for (String stockSymbol : STOCK_SYMBOLS) {
-//      URL url;
-//      try {
-//        url = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY"
-//                + "&outputsize=full"
-//                + "&symbol=" + STOCK_SYMBOLS
-//                + "&apikey=" + API_KEY
-//                + "&datatype=csv");
-//
-//        InputStream in = url.openStream();
-//        File file = new File(folder, STOCK_SYMBOLS + ".csv");
-//
-//        try (BufferedInputStream bis = new BufferedInputStream(in);
-//             FileOutputStream fos = new FileOutputStream(file)) {
-//          byte[] dataBuffer = new byte[1024];
-//          int bytesRead;
-//          while ((bytesRead = bis.read(dataBuffer, 0, 1024)) != -1) {
-//            fos.write(dataBuffer, 0, bytesRead);
-//          }
-//        } catch (IOException e) {
-//          System.err.println("Failed to write data for " + stockSymbol);
-//        }
-//        System.out.println("Data for " + stockSymbol + " has been saved to " + file.getAbsolutePath());
-//      } catch (IOException e) {
-//        throw new IllegalArgumentException("No price data found for " + stockSymbol);
-//      }
-//    }
-//  }
-
+  
   private static void deleteFolder(File folder) {
     File[] files = folder.listFiles();
     if (files != null) {
